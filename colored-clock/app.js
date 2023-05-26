020615b435e05d8f82e8cc1641cde318649e467d
vladg <vlad.grush.99@gmail.com>
2023-05-26 11:38:16 +0300

    Add chars to hex color background

--- colored-clock/app.js
+++ colored-clock/app.js
@@ -1,6 +1,13 @@
 const bodyTag = document.getElementsByTagName("body")[0];
 const clockDiv = document.getElementsByClassName("container-clock")[0];
+const hexNumbers = ["A", "B", "C", "D", "E", "F"];
 setInterval(() => {
+    const dayName = new Date().toLocaleString("en-US", { weekday: "long" });
+    const hexLettersFromDay = dayName.split("").map(char => {
+        if (hexNumbers.indexOf(char.toUpperCase()) > -1) {
+            return char.toUpperCase();
+        }
+    }).filter(item => item);
     let timeNow = new Date().toTimeString().split(" ")[0];
     const timeNowSplit = timeNow.split(":");
     const hourDiff = (99 - parseInt(timeNowSplit[0])).toString();
@@ -9,6 +16,25 @@ setInterval(() => {
     clockDiv.innerHTML = timeNow;
     clockDiv.style.color = "#" + hourDiff + minutesDiff + secondsDiff;
     let colorString = "#" + timeNow.toString().replace(":", "").replace(":", "");
+    let colorStringSplit = colorString.split("");
+    switch (hexLettersFromDay.length) {
+        case 0:
+            break;
+        case 1:
+            colorStringSplit[1] = hexLettersFromDay[0];
+            colorString = colorStringSplit.join("");
+            break;
+        case 2:
+            colorStringSplit[1] = hexLettersFromDay[0];
+            colorStringSplit[3] = hexLettersFromDay[1];
+            colorString = colorStringSplit.join("");
+            break;
+        case 3:
+            colorStringSplit[1] = hexLettersFromDay[0];
+            colorStringSplit[3] = hexLettersFromDay[1];
+            colorStringSplit[5] = hexLettersFromDay[2];
+            colorString = colorStringSplit.join("");
+    }
     console.log(colorString);
     bodyTag.style.backgroundColor = colorString;
 }, 1000);
