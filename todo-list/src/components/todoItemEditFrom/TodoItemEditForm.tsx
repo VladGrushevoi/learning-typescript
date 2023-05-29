26180516da512bb776e401f576f1fde59471e1eb
vladg <vlad.grush.99@gmail.com>
2023-05-29 19:24:01 +0300

    onchange to edit form

--- todo-list/src/components/todoItemEditFrom/TodoItemEditForm.tsx
+++ todo-list/src/components/todoItemEditFrom/TodoItemEditForm.tsx
@@ -1,24 +1,52 @@
+import { useInput } from "../../hooks/inputHook"
 import { TodoItem } from "../../models/TodoModel"
 
 interface TodoItemEditProps {
-    todoItem : TodoItem | undefined
+    todoItem: TodoItem | undefined
 }
 
-export const TodoItemEditForm = ({todoItem} : TodoItemEditProps) => {
+export const TodoItemEditForm = ({ todoItem }: TodoItemEditProps) => {
+
+    const titleInput = useInput(todoItem?.Title)
+    const descriptionInput = useInput(todoItem?.Description)
 
     return (
         <>
-            <input 
-                type="text"
-                value={todoItem && todoItem.Title}
-                className="border py-4 px-6 w-full outline-0" 
-            />
+            <form>
+                <label className="text-lg" htmlFor="editTitle">Title</label>
+                <input
+                    id="editTitle"
+                    type="text"
+                    placeholder="Enter the title..."
+                    {...titleInput}
+                    className="border py-4 px-6 m-4 w-2/3 block outline-1"
+                />
 
-            <button
-                className="py-2 px-4 font-bold left-0 border rounded-md bg-blue-400 h-1/2 text-center hover:text-green-900"            
-            >
-                Save
-            </button>
+                <label className="text-lg" htmlFor="editDescription">Description</label>
+                <input
+                    id="editDescription"
+                    type="text"
+                    placeholder="Enter the description..."
+                    {...descriptionInput}
+                    className="border py-4 px-6 m-4 w-2/3 block outline-1"
+                />
+
+                <label className="text-lg" htmlFor="edotIsDone">Is done</label>
+                <input
+                    id="edotIsDone"
+                    type="checkbox"
+                    placeholder="Enter the title..."
+                    checked={todoItem && todoItem.isDone}
+                    className="border py-4 px-6 m-4"
+                />
+
+                <button
+                    type="submit"
+                    className="py-2 px-4 w-1/3 m-auto block font-bold left-0 border rounded-md bg-blue-400 h-1/2 text-center hover:text-yellow-400"
+                >
+                    Save
+                </button>
+            </form>
         </>
     )
 }
---
+++ todo-list/src/hooks/inputHook.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export const useInput = (initData : string | undefined) => {
+    const [value, setValue] = useState(initData);
+
+    return {
+        value: value,
+        onChange : (event : React.ChangeEvent<HTMLInputElement>) : void => setValue(event.currentTarget.value), 
+    }
+}
