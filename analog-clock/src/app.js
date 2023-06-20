99dd31c766fc35702d66b8b96bc6860af06700c6
vladg <vlad.grush.99@gmail.com>
2023-06-20 14:24:09 +0300

    analog-clock done

--- analog-clock/src/app.js
+++ analog-clock/src/app.js
@@ -1,1 +1,18 @@
+const hours = document.getElementById("hour");
+const minutes = document.getElementById("minutes");
+const secondes = document.getElementById("secondes");
+function SetDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondes.style.transform = `rotate(${secondsDegrees}deg)`;
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minutes.style.transform = `rotate(${minsDegrees}deg)`;
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    hours.style.transform = `rotate(${hourDegrees}deg)`;
+}
+SetDate();
+setInterval(SetDate, 1000);
 //# sourceMappingURL=app.js.map
