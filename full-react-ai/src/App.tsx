f4c77b1abb6cae6784695a87f9d26d09e22f4cfc
vladg <vlad.grush.99@gmail.com>
2023-07-01 13:15:39 +0300

    add tailwind css

--- full-react-ai/src/App.tsx
+++ full-react-ai/src/App.tsx
@@ -1,22 +1,13 @@
-import { Button, Card, Spinner, Stack } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Card style={{width: '20%'}}>
-      <Card.Header>
-        <Card.Title>PINUS</Card.Title>
-      </Card.Header>
-      <Card.Body>
-        <Card.Img />
-        <Card.Text>
-          eckawjeabejabfjawbeeckawjeabejabfjawbeeckawjeabejabfjawbeeckawjeabejabfjawbeeckawjeabejabfjawbeeckawjeabejabfjawbeeckawjeabejabfjawbe
-        </Card.Text>
-        <Button variant='primary'>SOMETHING</Button>
-      </Card.Body>
-      </Card>
+      <Container >
+
+      </Container>
     </>
   )
 }
