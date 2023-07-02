79a1454aca653d85a16379fef3709b9461663ffb
vladg <vlad.grush.99@gmail.com>
2023-07-02 14:36:41 +0300

    add tailwind css

--- e-journal/src/App.tsx
+++ e-journal/src/App.tsx
@@ -5,22 +5,24 @@ function App() {
 
   return (
     <>
-      <Container>
-        <Row>
-          <h1 className="">HEADER</h1>
+      <Container className='h-full' fluid>
+        <Row className="bg-gradient bg-primary text-center h-1/5">
+          <h1 >HEADER</h1>
         </Row>
-        <Row>
-          <Col>
-
+        <Row className='h-3/5'>
+          <Col xs={2} md={2} className='bg-blue-300'>
+            <h1>NAVIGATION</h1>
           </Col>
-          <Col>
-          
+          <Col xs={6} md={8} className='bg-black'>
+            <h1 >MAIN</h1>
           </Col>
-          <Col>
-          
+          <Col className='bg-green-500'>
+          <h1 >SOMETHING</h1>
           </Col>
         </Row>
-        <Row></Row>
+        <Row className='bg-gradient bg-warning text-center'>
+          <h1 >ПІДВАЛ</h1>
+        </Row>
       </Container>
     </>
   )
