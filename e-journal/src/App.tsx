cb0cce4d6832167d33d8b279fdce28785d3a10a3
vladg <vlad.grush.99@gmail.com>
2023-07-02 13:12:31 +0300

    add bootstrap

---
+++ e-journal/src/App.tsx
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <h1 className="">HEADER</h1>
+        </Row>
+        <Row>
+          <Col>
+
+          </Col>
+          <Col>
+          
+          </Col>
+          <Col>
+          
+          </Col>
+        </Row>
+        <Row></Row>
+      </Container>
+    </>
+  )
+}
+
+export default App
