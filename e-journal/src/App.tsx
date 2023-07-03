05755a3d1b22a5eac15ea11d49a9cdd90925c636
vladg <vlad.grush.99@gmail.com>
2023-07-03 10:57:53 +0300

    base position component

--- e-journal/src/App.tsx
+++ e-journal/src/App.tsx
@@ -5,22 +5,22 @@ function App() {
 
   return (
     <>
-      <Container className='h-full' fluid>
-        <Row className="bg-gradient bg-primary text-center h-1/5">
-          <h1 >HEADER</h1>
+      <Container className='h-full bg-pink-200 min-h-screen' fluid>
+        <Row className="bg-gradient bg-primary text-center h-1/5  rounded-b-2xl">
+          <h1>HEADER</h1>
         </Row>
-        <Row className='h-3/5'>
-          <Col xs={2} md={2} className='bg-blue-300'>
+        <Row className=' px-6 py-8 text-center gap-4'>
+          <Col md={3} sm={4} xl={4} xxl={2} className='bg-blue-300 h-[20vh]'>
             <h1>NAVIGATION</h1>
           </Col>
-          <Col xs={6} md={8} className='bg-black'>
-            <h1 >MAIN</h1>
+          <Col  className='bg-black'>
+            <h1 className='h-[45vh]'> MAIN</h1>
           </Col>
-          <Col className='bg-green-500'>
-          <h1 >SOMETHING</h1>
+          <Col lg={12} className='bg-green-500'>
+          <h1 className='h-[15vh]' >SOMETHING</h1>
           </Col>
         </Row>
-        <Row className='bg-gradient bg-warning text-center'>
+        <Row className='bg-gradient text-center fixed bottom-0 w-full rounded-t-2xl'>
           <h1 >ПІДВАЛ</h1>
         </Row>
       </Container>
