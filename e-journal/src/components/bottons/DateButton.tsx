50c1c4ce26128e78a851571ee834dab2c5e4f2cc
vladg <vlad.grush.99@gmail.com>
2023-07-04 11:54:00 +0300

    try to responsive layout

---
+++ e-journal/src/components/bottons/DateButton.tsx
@@ -0,0 +1,12 @@
+import { Button, Nav } from "react-bootstrap"
+
+export const DateButton = () => {
+
+    return (
+        <>
+                <Nav.Link>
+                    <Button className="flex-wrap w-24">Понеділок, 03.07.2023</Button>
+                </Nav.Link> 
+        </>
+    )
+}
--- e-journal/src/components/pages/MainPage/MainPage.tsx
+++ e-journal/src/components/pages/MainPage/MainPage.tsx
@@ -1,13 +1,71 @@
-import { Col, Row } from "react-bootstrap"
+import { Button, Col, Nav, Row, Table } from "react-bootstrap"
+import { DateButton } from "../../bottons/DateButton"
 
 export const MainPage = () => {
 
     return (
         <>
-            <Row className=' px-6 py-8 text-center gap-3 h-[75vh]'>
-                <Col  className='border-2 rounded-2xl border-orange-300 shadow-lg h-[70%]'>
+            <Row className=' px-6 py-8 text-center gap-3 h-[80vh]'>
+                <Col className='border-2 rounded-2xl border-orange-300 shadow-lg'>
+                    <Row className="gap-4 p-6">
+                        <Col className="">
+                            <Nav className="">
+                                <DateButton />
+                                <DateButton />
+                                <DateButton />
+                                <DateButton />
+                                <DateButton />
+                                <DateButton />
+                                <DateButton />
+                            </Nav>
+                        </Col>
+                        <Col md={8} xs={8} lg={9} className="border bg-slate-200">
+                            <Table className="text-center" striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <th>ЧАС</th>
+                                        <th>Статус</th>
+                                        <th>Тут нопка</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>9:00</td>
+                                        <td>Вільно</td>
+                                        <td><Button variant="success">Записатися</Button></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>10:00</td>
+                                        <td>Записано</td>
+                                        <td><Button variant="error">Записатися</Button></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>11:00</td>
+                                        <td>Вільно</td>
+                                        <td><Button variant="success">Записатися</Button></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>12:00</td>
+                                        <td>Вільно</td>
+                                        <td><Button variant="success">Записатися</Button></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>13:00</td>
+                                        <td>Вільно</td>
+                                        <td><Button variant="success">Записатися</Button></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
                 </Col>
-                <Col lg={3} className='bg-green-300 rounded-lg h-[30%]'>
+                <Col lg={2} className='bg-green-300 rounded-lg'>
                 </Col>
             </Row>
         </>
