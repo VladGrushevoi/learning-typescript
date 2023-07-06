a3bb37e9d2673f15a7de8554b1254b19cccf2822
vladg <vlad.grush.99@gmail.com>
2023-07-06 18:50:58 +0300

    init structure

--- cretik-spinWheel/src/App.tsx
+++ cretik-spinWheel/src/App.tsx
@@ -1,10 +1,54 @@
+import { Container, Row, Col, Stack, Button, InputGroup, Form } from 'react-bootstrap'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className='underline'>Allo</h1>
+      <Container className='bg-purple-900 h-[100vh]' fluid>
+        <Row className='h-[100vh]'>
+          <Col className='border-r-2 '>
+            <Stack direction='vertical'>
+              <Row>
+                <Col md={6}>ЛОХ1</Col>
+                <Col>200pts</Col>
+              </Row>
+              <Row>
+                <Col md={6}>ЛОХ2</Col>
+                <Col>200pts</Col>
+              </Row>
+              <Row>
+                <Col md={6}>ЛОХ2</Col>
+                <Col>200pts</Col>
+              </Row>
+            </Stack>
+          </Col>
+          <Col md={10} className='gap-4'>
+            <Row md={10} className=''>
+              <Col>
+                <div className='rounded-full border w-[80vh] h-[80vh] m-auto'></div>
+              </Col>
+            </Row>
+            <Row className='border h-[20vh]'>
+              <Col>
+                <InputGroup size="lg" className='mt-5'>
+                  <InputGroup.Text id="inputGroup-sizing-lg">ЛОХ</InputGroup.Text>
+                  <Form.Control
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                  />
+                  <InputGroup.Text id="inputGroup-sizing-lg">$</InputGroup.Text>
+                  <Form.Control
+                    aria-label="Large"
+                    aria-describedby="inputGroup-sizing-sm"
+                  />
+                  <Button variant="outline-secondary">ДОДАТИ</Button>
+                </InputGroup>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </>
   )
 }
