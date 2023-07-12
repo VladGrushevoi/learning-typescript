acb4ae1e51abd45ff2caf20a671c35325eb2c488
vladg <vlad.grush.99@gmail.com>
2023-07-12 20:42:09 +0300

    create base layout

--- e-journal/src/components/pages/ProfilePage/ProfilePage.tsx
+++ e-journal/src/components/pages/ProfilePage/ProfilePage.tsx
@@ -1,12 +1,32 @@
+import { Col, Container, Row } from "react-bootstrap"
+
 interface ProfilePageProps {
 
 }
 
-export const ProfilePage = ({} : ProfilePageProps) => {
+export const ProfilePage = ({ }: ProfilePageProps) => {
 
     return (
         <>
-            <h1>PROFILE PAGE</h1>
+            <Container fluid className="px-6 py-4">
+                <Row>
+                    <Col md={5} className="border-2 border-black rounded-lg h-[80vh] md:h-[50vh]" >
+                        <h1
+                            className="text-center"
+                        >
+                            ОСОБИСТА ІНФОРМАЦІЯ
+                        </h1>
+                    </Col>
+                    <Col md={2} xs={2} sm={2} className="my-4 md:m-0"></Col>
+                    <Col md={5} className="border-2 border-green-400 rounded-lg h-[80vh] md:h-[50vh]">
+                        <h1
+                            className="text-center"
+                        >
+                            ІСТОРІЯ ЗАПИСІВ
+                        </h1>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
--- e-journal/tailwind.config.js
+++ e-journal/tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{html,js, tsx, ts}',
+    './components/**/*.{html,js, ts, tsx}',
   ],
   theme: {
     extend: {},
