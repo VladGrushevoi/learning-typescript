9b103c885bca7fb14d820e739e4bbb517f2ac926
vladg <vlad.grush.99@gmail.com>
2023-07-27 11:34:20 +0300

    change component for displaying

--- e-journal/src/components/ConfiguratorWorkDays/ConfiguratorWorkDays.tsx
+++ e-journal/src/components/ConfiguratorWorkDays/ConfiguratorWorkDays.tsx
@@ -1,53 +1,47 @@
-import { Tab, Tabs } from "react-bootstrap"
-import { AddWorkHourForm } from "./AddWorkHourForm"
+import { Button, Col, Row } from "react-bootstrap";
 import { useFakeWorkHour } from "../../fake-data/fake-workHour"
-import { ListWorkHour } from "./ListWorkHour"
+import { useState } from "react";
+import { ListWorkHour } from "./ListWorkHour";
+import { AddWorkHourForm } from "./AddWorkHourForm";
 
 interface ConfiGuratorWorkDaysProps {
 
 }
 
+const days = ["Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота", "Неділя",]
+
 export const ConfiGuratorWorkDays = ({ }: ConfiGuratorWorkDaysProps) => {
 
     const { addNewWorkHour, workHour } = useFakeWorkHour();
+    const [chooseDayIndex, setDayIndex] = useState(0);
+    const handleChooseDay = (dayIndex: number) => {
+        setDayIndex(dayIndex)
+    }
 
     return (
         <>
-            <Tabs
-                defaultActiveKey="Monday"
-                id="justify-tab-example"
-                className="mb-3 border-b-2 border-black"
-                justify
-            >
-                <Tab
-                    eventKey="Monday"
-                    title="Понеділок"
-                    className="px-4 text-lg"
-                >
-                    <ListWorkHour workHours={workHour[0]} dayIndex={0} />
-                    <AddWorkHourForm addNewHour={addNewWorkHour} dayIndex={0} />
-                </Tab>
-                <Tab eventKey="Wednesday" title="Середа">
-                    <ListWorkHour workHours={workHour[2]} dayIndex={2} />
-                    <AddWorkHourForm addNewHour={addNewWorkHour} dayIndex={2} />
-                </Tab>
-                <Tab eventKey="Thursday" title="Четвер" >
-                    <ListWorkHour workHours={workHour[3]} dayIndex={3} />
-                    <AddWorkHourForm addNewHour={addNewWorkHour} dayIndex={3} />
-                </Tab>
-                <Tab eventKey="Friday" title="П'ятниця" >
-                    <ListWorkHour workHours={workHour[4]} dayIndex={4} />
-                    <AddWorkHourForm addNewHour={addNewWorkHour} dayIndex={4} />
-                </Tab>
-                <Tab eventKey="Saturday" title="Субота" >
-                    <ListWorkHour workHours={workHour[5]} dayIndex={5} />
-                    <AddWorkHourForm addNewHour={addNewWorkHour} dayIndex={5} />
-                </Tab>
-                <Tab eventKey="Sunday" title="Неділя" >
-                    <ListWorkHour workHours={workHour[6]} dayIndex={6} />
-                    <AddWorkHourForm addNewHour={addNewWorkHour} dayIndex={6} />
-                </Tab>
-            </Tabs>
+            <Row className="gap-2">
+                {
+                    days.map((day, index) => {
+                        return (
+                            <Col>
+                                <Button 
+                                    key={index+day} 
+                                    onClick={() => handleChooseDay(index)}
+                                    className=""
+                                    variant={chooseDayIndex !== index ? 'secondary' : 'success'}
+                                >
+                                    {day}
+                                </Button>
+                            </Col>
+                        )
+                    })
+                }
+            </Row>
+            <Row>
+                <ListWorkHour dayIndex={chooseDayIndex} workHours={workHour[chooseDayIndex]} />
+                <AddWorkHourForm addNewHour={addNewWorkHour} dayIndex={chooseDayIndex} />
+            </Row>
         </>
     )
 }
