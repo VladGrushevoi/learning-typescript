130031039cb6d0fde0eb408d6953b8d03b9ccd5c
vladg <vlad.grush.99@gmail.com>
2023-08-02 14:54:26 +0300

    add music link

--- dota2-rnd-team-picker/src/App.tsx
+++ dota2-rnd-team-picker/src/App.tsx
@@ -13,7 +13,8 @@ import { Roulette } from './components/Roulette';
 
 const musicList = [
   "http://stream.zeno.fm/71ntub27u18uv",
-  "https://download.xn--41a.wiki/listen_6555612_456244947_1_e3387fcd83c38b14e8e79904a0a05b91/Huby-Danika%20House.mp3"
+  "https://download.xn--41a.wiki/cache/2/66d/6555612_456244947.mp3?filename=Huby-Danika%20House.mp3",
+  "https://download.xn--41a.wiki/cache/2/7e1/252581702_456243505.mp3?filename=%D0%9F%D0%BE%D0%BB%D0%B5%20%D0%A7%D1%83%D0%B4%D0%B5%D1%81-%D0%97%D0%B2%D1%83%D0%BA%20%D0%B2%D1%80%D0%B0%D1%89%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%B1%D0%B0%D1%80%D0%B0%D0%B1%D0%B0%D0%BD%D0%B0%20%D0%B2%20%D0%9F%D0%BE%D0%BB%D0%B5%20%D0%A7%D1%83%D0%B4%D0%B5%D1%81.mp3"
 ]
 
 function App() {
