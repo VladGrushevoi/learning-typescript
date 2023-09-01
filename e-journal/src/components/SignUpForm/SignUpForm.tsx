db1629a3112a4d792500674e743e750549f887dd
vladg <vlad.grush.99@gmail.com>
2023-09-01 11:10:36 +0300

    add show password to register form

--- e-journal/src/components/SignUpForm/SignUpForm.tsx
+++ e-journal/src/components/SignUpForm/SignUpForm.tsx
@@ -1,16 +1,19 @@
+import { useState } from 'react'
 import { Button, Form, InputGroup, Row } from "react-bootstrap"
-import { useSignUpForm } from "./usesignUpForm"
+import { useSignUpForm } from "./useSignUpForm";
+import { BrightnessHighFill } from "react-bootstrap-icons";
 
 
 export const SignUpForm = () => {
-
+    const [showPassword, setShowPassword] = useState<boolean>(false)
     const {
         confirmPassword,
         firstNameInput,
         lastNameInput,
         passwordInput,
         phoneNumberInput,
-        registerHandle
+        registerHandle,
+        showRegisterResult
     } = useSignUpForm();
 
     return (
@@ -37,15 +40,23 @@ export const SignUpForm = () => {
                     <Form.Label>Придумайте пароль</Form.Label>
                     <InputGroup>
                         <InputGroup.Text id="basic-addon1">Пароль</InputGroup.Text>
-                        <Form.Control {...passwordInput} type="password" placeholder="Введіть пароль" />
-                        <InputGroup.Text id="basic-addon1">Повторіть свій пароль</InputGroup.Text>
-                        <Form.Control {...confirmPassword} type="password" placeholder="Повторно введіть пароль" />
+                        <Form.Control {...passwordInput} type={showPassword ? 'text' : "password"}  placeholder="Введіть пароль" />
+                        <InputGroup.Text id="basic-addon1">Повторіть</InputGroup.Text>
+                        <Form.Control {...confirmPassword} type={showPassword ? 'text' : "password"} placeholder="Повторно введіть пароль" />
+                        <InputGroup.Text id="basic-addon1">
+                            <Button className="border-0 bg-transparent p-0" onClick={() => setShowPassword(!showPassword)}>
+                                <BrightnessHighFill  className={showPassword ? `fill-red-500`:`fill-green-500`}/>
+                            </Button>
+                        </InputGroup.Text>
                     </InputGroup>
                 </Form.Group>
                 <Row>
                     <Button variant="primary" type="submit" className="">
                         Зареєструватися
                     </Button>
+                </Row>
+                <Row>
+                    {showRegisterResult}
                 </Row>
             </Form>
         </>
--- e-journal/src/components/SignUpForm/useSignUpForm.tsx
+++ e-journal/src/components/SignUpForm/useSignUpForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { appAxios } from "../../appAxios/appAxios";
 import { useInput } from "../../hooks/useInput"
 import { UserRole } from "../../types/User";
 
@@ -10,15 +12,19 @@ interface UserRegisterInfo {
 }
 
 export const useSignUpForm = () => {
+    const [showRegisterResult, setResult] = useState("")
     const firstNameInput = useInput("", "FirstName");
     const lastNameInput = useInput("", "LastName");
     const phoneNumberInput = useInput("","Phone")
     const passwordInput = useInput("", "Password");
     const confirmPassword = useInput("", "ConfirmPasword");
 
-    const registerHandle = (event: React.FormEvent) => {
+    const registerHandle = async (event: React.FormEvent) => {
         event.preventDefault();
-
+        if(passwordInput.value !== confirmPassword.value){
+            setResult("Паролі не співпадають, перевірте правильність вводу");
+            return;
+        }
         const userRegisterInfo : UserRegisterInfo = {
             firstName: firstNameInput.value,
             lastName: lastNameInput.value,
@@ -26,8 +32,12 @@ export const useSignUpForm = () => {
             password: passwordInput.value,
             role: UserRole.User
         }
-
-        console.log(userRegisterInfo)
+        const response = await appAxios.post("/user/register", JSON.stringify(userRegisterInfo))
+        if(response.status !== 200){
+            setResult("Помилка. Користувач не був зареєстрований. Будь ласка провірте дані")
+            return;
+        }
+        setResult("Користувач успішно зареєстрований. Будь-ласка авторизуйтеся")
     }
 
     return {
@@ -36,6 +46,7 @@ export const useSignUpForm = () => {
         phoneNumberInput,
         passwordInput,
         confirmPassword,
-        registerHandle
+        registerHandle,
+        showRegisterResult
     }
 }
